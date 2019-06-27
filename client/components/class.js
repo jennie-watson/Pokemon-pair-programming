575f8427effe1c6c066f478c56522689217d08a8
Celia Bailey <cabailey@xtra.co.nz>
2019-06-28 10:57:17 +1200

    made array of classmates

---
+++ client/components/class.js
@@ -0,0 +1,1 @@
+export const matai = ['tamari', 'noel', 'rahul', 'jennie', 'andre', 'ash', 'taine', 'jared', 'joanna', 'anisha', 'davidb', 'davidm', 'raaya', 'kete', 'raaya', 'ruslan']
--- client/components/test.jsx
+++ client/components/test.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { matai } from './class'
 const api = require('../api')
 
 export default class test extends Component {
+  state = {
+    class: matai
+  }
   componentDidMount () {
     api.getChars()
+    console.log(matai)
   }
   render () {
     return (
